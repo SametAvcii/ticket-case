7f2cf9172da4833ac6c422cdd2a874a24f5609d3
sametavcii <sametavc05@gmail.com>
2022-11-14 20:16:14 +0300

    feat open api swagger added

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,35 @@ import (
 
 	"samet-avci/gowit/router"
 
+	_ "samet-avci/gowit/docs"
+
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
 func init() {
 	db.Connect()
 }
 
+// @title Gowit API Documentation
+// @version 1.0
+// @description This is a server api docs for
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Samet Avcı
+// @contact.url https://www.linkedin.com/in/samet-avci/
+// @contact.email sametavc05@gmail.com
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
 func main() {
 
 	db := db.Connect()
 	e := echo.New()
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	router.Init(e, db)
 	e.Logger.Fatal(e.Start(":8080"))
 
